a71e38f99c60528e0c990613d4450e09f415ec53
Mostafizur Rahman Nayem <mrnayem4403@gmail.com>
2022-03-19 22:04:06 +0600

    Travel Web site Made by me

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+import Navbar from "./Componnent/Navbar";
+import './App.css'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import Home from "./Componnent/page/Home";
+import Services from "./Componnent/page/Services";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/services' component={Services}></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
